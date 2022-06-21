de4bd51d2074a088287c9003b860edc7653a061a
Balazs Hirth <balazs.hirth@gmail.com>
2022-06-21 15:02:51 +0200

    Add pytest configuration

--- test_api.py
+++ test_api.py
@@ -1,10 +1,48 @@
 import json
-import api
-from main import app
+import orm
+from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.orm import declarative_base, relationship
+import pytest
 
-def test_get_pet():
-    url = '/pets/'
-    client = app.app.test_client()
-    resp = client.get(url)
-    # data = json.loads(resp.data)
-    assert resp.status_code == 200
+
+# def test_get_pet():
+#     url = '/pets'
+#     client = application.test_client()
+#     resp = client.get(url)
+#     print(resp.text)
+#     assert resp.status_code == 200
+
+
+
+@pytest.fixture(scope="session")
+def connection():
+    engine = create_engine('sqlite:///test.db')
+    return engine.connect()
+
+
+@pytest.fixture(scope="session")
+def setup_database(connection):
+    orm.Base.metadata.bind = connection
+    orm.Base.metadata.create_all()
+
+    yield
+
+    orm.Base.metadata.drop_all()
+
+
+@pytest.fixture
+def db_session(setup_database, connection):
+    transaction = connection.begin()
+    yield scoped_session(
+        sessionmaker(autocommit=False, autoflush=False, bind=connection)
+    )
+    transaction.rollback()
+
+
+def test_user_created(db_session):
+    db_session.add(orm.Person(first_name="Proba", email = 'proba@email'))
+    db_session.commit()
+    assert len(db_session.query(orm.Person).all()) == 1
